2e8aedd5eb278d6e6488515150f8936b5d6c37ea
roger <grarogccee@gmail.com>
2024-12-19 22:27:57 -0500

    delete validation class , use reactive forms

--- src/app/models/product.dto.ts
+++ src/app/models/product.dto.ts
@@ -1,50 +1,15 @@
-import {
-  IsDateString,
-  IsNotEmpty,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
-import { Transform } from 'class-transformer';
+import { IsDateString, IsNotEmpty } from 'class-validator';
 import { ProductInterface } from '../interfaces/product.interface';
-import {
-  IsReleaseDateValid,
-  IsRevisionDateValid,
-} from '../utils/class-validator';
 
 export class ProductDTO implements ProductInterface {
-  @IsNotEmpty()
-  @MinLength(3)
-  @MaxLength(10)
   id: string;
-
-  @IsNotEmpty()
-  @MinLength(5)
-  @MaxLength(100)
   name: string;
-
-  @IsNotEmpty()
-  @MinLength(10)
-  @MaxLength(200)
   description: string;
-
-  @IsNotEmpty()
   logo: string;
-
   @IsNotEmpty()
   @IsDateString()
-  @IsReleaseDateValid({
-    message: 'La fecha de liberación debe ser igual o mayor a la fecha actual.',
-  })
-  @Transform(({ value }) => new Date(value), { toClassOnly: true })
   date_release: Date;
 
-  @IsNotEmpty()
-  @IsDateString()
-  @IsRevisionDateValid({
-    message:
-      'La fecha de revisión debe ser exactamente un año posterior a la fecha de liberación.',
-  })
-  @Transform(({ value }) => new Date(value), { toClassOnly: true })
   date_revision: Date;
 
   constructor(
